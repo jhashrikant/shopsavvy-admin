1dca3d7604624662bb79d1ea62d140253ba15f7d
Shrikant Jha <87967393+jhashrikant@users.noreply.github.com>
2024-02-28 19:08:13 +0530

    commit

--- app/layout.js
+++ app/layout.js
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
 
 	return (
 		<html lang="en">
-			<body>
+			<body className={inter.className}>
 				<ProductProvider>
 					<WrapperComponent>
 						{children}
